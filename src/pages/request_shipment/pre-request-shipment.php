20ecb7925ef8d91b79362f9af55a374a2bc37621
Shutirtha Roy <shutirtha.roy1@gmail.com>
2024-09-07 14:50:35 +1000

    registration session added

--- src/pages/request_shipment/pre-request-shipment.php
+++ src/pages/request_shipment/pre-request-shipment.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
+     header("location: ../account/login.php");
+     exit;
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
